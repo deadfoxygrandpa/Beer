d52056803b50e37606c14e5d2318210430d726a6
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-01-10 16:32:43 +0800

    Rescaled drunk effects overall

--- SpecialEffects.elm
+++ SpecialEffects.elm
@@ -18,12 +18,13 @@ trans x y = Transform2D.multiply (shear x) (scale y)
 distort : Model.SpecialEffect
 distort environment element = 
     let (w, h) = environment.windowSize
-        sx = environment.state.person.bac
-        sy = environment.state.person.bac
+        factor = drunkFactor environment.state.person.bac
+        sx = factor
+        sy = factor * 0.5
         x = environment.time
         y = environment.time
     in collage w h <|
-    [ groupTransform (trans (sx * (sin <| x/60)) ((0.95 + (0.5 * sy * (sin <| y/50))))) [toForm element] ]
+    [ groupTransform (trans (sx * (sin <| x/60)) ((0.95 + (sy * (sin <| y/50))))) [toForm element] ]
 
 plain : Model.SpecialEffect
 plain environment element =
@@ -35,10 +36,13 @@ fuzzy environment element =
     let form = toForm element
         (w, h) = environment.windowSize
         t = environment.time
-        bac = environment.state.person.bac
-        x = 50 * bac * (sin <| t / 15)
-        y = 20 * bac * (cos <| t / 10)
+        factor = clamp 0 100 <| environment.state.person.bac - 0.01
+        x = 50 * factor * (sin <| t / 15)
+        y = 20 * factor * (cos <| t / 10)
     in collage w h [ alpha 0.7 . move (-x, y) <| form, alpha 0.7 . move (x, y) <| form ] 
 
 theBest : Model.SpecialEffect
 theBest environment = distort environment . fuzzy environment
+
+drunkFactor : Float -> Float
+drunkFactor bac = bac^2
