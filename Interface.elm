4db4bb63a610ffb2dc4866f23f36e3f4b13d5306
Alex Neslusan <deadfoxygrandpa@gmail.com>
2015-01-18 23:39:37 +0800

    Fix missing variables in Interface.elm

--- Interface.elm
+++ Interface.elm
@@ -2,52 +2,63 @@ module Interface where
 
 -- Standard Library imports
 import Graphics.Input as Input
+import Signal
 import String
 import Window
 import Char
 import Keyboard
+import List (map, map2, take)
+import Color (Color, white, black, lightGrey, grey)
+import Graphics.Element (Element, container, middle, color, flow, right, down, heightOf, midTopAt, relative, width, layers, bottomLeft)
+import Text (plainText)
+import Time (Time, inHours, inMinutes, inSeconds)
+import Maybe (Maybe (..), withDefault)
 
 -- Project imports
 import SpecialEffects
 import Model
 import Constants
 
+import Signal.Discrete as Discrete
+
+zip = map2 (,)
+
 makeButtons : String -> (Int, Int) -> List String -> (List Element, Signal String)
 makeButtons initial dimensions names =
-    let inputs = map (\_ -> Input.input initial) names
-        elems  = map (\(i, n) -> Input.customButton i.handle n (box n dimensions white) (box n dimensions lightGrey) (box n dimensions grey)) (zip inputs names)
-    in  (elems, merges <| map .signal inputs)
+    let inputs = map (\_ -> Signal.channel initial) names
+        elems  = map (\(i, n) -> Input.customButton (Signal.send i n) (box n dimensions white) (box n dimensions lightGrey) (box n dimensions grey)) (zip inputs names)
+    in  (elems, Signal.mergeMany <| map Signal.subscribe inputs)
 
 keyPressed : Char -> Signal ()
 keyPressed char = keyCodePressed <| Char.toCode char
 
 keyCodePressed : Char.KeyCode -> Signal ()
-keyCodePressed code = (\_ -> ()) <~ (dropRepeats <| ((\n -> n `div` 2) <~ (count <| Keyboard.isDown code)))
+keyCodePressed code = Discrete.whenChangeTo True <| Keyboard.isDown code
 
 latch : Signal a -> Signal Bool
-latch sig = dropRepeats <| (\x -> mod x 2 == 0) <~ (count sig)
+latch sig = Discrete.folde not False <| Discrete.whenChange sig
 
 box : String -> (Int, Int) -> Color -> Element
-box s (w, h) c = color black . container w h middle
-               . color c . container (w-2) (h-2) middle <| plainText s
+box s (w, h) c = color black << container w h middle
+               << color c << container (w-2) (h-2) middle <| plainText s
 
 (timeAccelerationButtons, timeAcceleration) = makeButtons "1" (40, 30) ["1", "2", "60", "600"]
 
-chug = Input.input ()
-sip = Input.input ()
-gulp = Input.input ()
-urinate = Input.input ()
-order = Input.input ()
-order2 = Input.input ()
-(chugButton, chugClicks)       = (Input.button chug.handle    () "slam back a brewski" , chug.signal)
-(sipButton, sipClicks)         = (Input.button sip.handle     () "sip your beer"       , sip.signal)
-(gulpButton, gulpClicks)       = (Input.button gulp.handle    () "gulp down some beer" , gulp.signal)
-(urinateButton, urinateClicks) = (Input.button urinate.handle () "urinate"             , urinate.signal)
-(orderButton, orderClicks)     = (Input.button order.handle   () "another pint, please", order.signal)
-(orderButton2, orderClicks2)   = (Input.button order2.handle  () "order from the menu" , order2.signal)
+chug = Signal.channel ()
+sip = Signal.channel ()
+gulp = Signal.channel ()
+urinate = Signal.channel ()
+order = Signal.channel ()
+order2 = Signal.channel ()
+(chugButton, chugClicks)       = (Input.button (Signal.send chug    ()) "slam back a brewski" , Signal.subscribe chug)
+(sipButton, sipClicks)         = (Input.button (Signal.send sip     ()) "sip your beer"       , Signal.subscribe sip)
+(gulpButton, gulpClicks)       = (Input.button (Signal.send gulp    ()) "gulp down some beer" , Signal.subscribe gulp)
+(urinateButton, urinateClicks) = (Input.button (Signal.send urinate ()) "urinate"             , Signal.subscribe urinate)
+(orderButton, orderClicks)     = (Input.button (Signal.send order   ()) "another pint, please", Signal.subscribe order)
+(orderButton2, orderClicks2)   = (Input.button (Signal.send order2  ()) "order from the menu" , Signal.subscribe order2)
 
 timeFactor : Signal Time -> Signal Time
-timeFactor time = (\x t -> inHours (1000 * (maybe 1 id <| String.toFloat x) / (1000 / t))) <~ timeAcceleration ~ time
+timeFactor time = Signal.map2 (\x t -> inHours (1000 * (withDefault 1 <| String.toFloat x) / (1000 / t))) timeAcceleration time
 
 instructions : Element
 instructions =
@@ -67,33 +78,33 @@ instructions =
 
 render : Model.State -> (Int, Int) -> Int -> Element
 render state (w, h) seed =
-    let messages   = container w h (midTopAt (relative 0.5) (relative 0.7)) <| flow down <| map (plainText . .msg) (take 5 state.messages)
+    let messages   = container w h (midTopAt (relative 0.5) (relative 0.7)) <| flow down <| map (plainText << .msg) (take 5 state.messages)
         gameScreen = container w h middle <|
                      flow down [ flow right [chugButton, plainText " time acceleration: ", flow right timeAccelerationButtons]
-                               , flow right [plainText "you are a ", plainText . String.left 5 . show <| state.person.weight, plainText "kg ", plainText . show <| state.person.sex]
-                               , flow right [plainText "your current beer of choice is ", plainText . show <| (snd state.person.beers).name]
-                               , flow right [plainText "of which you have ", width 35 <| plainText . show <| fst state.person.beers, plainText " ml left in the glass"]
-                               , flow right [plainText "you got ", plainText . String.left 4 . show <| state.person.alc, plainText " grams of unabsorbed ethanol in ur belly"]
-                               , flow right [plainText "ur bac is: ", plainText . String.left 6 . show <| state.person.bac]
+                               , flow right [plainText "you are a ", plainText << String.left 5 << toString <| state.person.weight, plainText "kg ", plainText << toString <| state.person.sex]
+                               , flow right [plainText "your current beer of choice is ", plainText << toString <| (snd state.person.beers).name]
+                               , flow right [plainText "of which you have ", width 35 <| plainText << toString <| fst state.person.beers, plainText " ml left in the glass"]
+                               , flow right [plainText "you got ", plainText << String.left 4 << toString <| state.person.alc, plainText " grams of unabsorbed ethanol in ur belly"]
+                               , flow right [plainText "ur bac is: ", plainText << String.left 6 << toString <| state.person.bac]
                                , plainText <| peeDisplay state.person.urine state.person.wetSelf ++ (if state.person.urinating then " (you are peeing)" else "")
-                               , flow right [plainText "you've had ", plainText . String.left 4 . show <| state.drinks, plainText " beers"]
+                               , flow right [plainText "you've had ", plainText << String.left 4 << toString <| state.drinks, plainText " beers"]
                                , flow right [plainText "u been at the bar for: ", plainText <| timeDisplay state.elapsed]
                                , flow right [sipButton, gulpButton, urinateButton]
                                , flow right [orderButton, orderButton2]
                                ]
         distorted = layers [messages, gameScreen]
-    in  layers [instructions, distorted, container w h bottomLeft (plainText <| "random seed: " ++ show seed)]
+    in  layers [instructions, distorted, container w h bottomLeft (plainText <| "random seed: " ++ toString seed)]
 
 gameScreen : Model.State -> (Int, Int) -> Element
 gameScreen state (w, h) = container w h middle <|
     flow down [ flow right [chugButton, plainText " time acceleration: ", flow right timeAccelerationButtons]
-              , flow right [plainText "you are an ", plainText . show <| state.person.weight, plainText "kg ", plainText . show <| state.person.sex]
-              , flow right [plainText "your current beer of choice is ", plainText . show <| (snd state.person.beers).name]
-              , flow right [plainText "of which you have ", plainText . show <| fst state.person.beers, plainText " ml left in the glass"]
-              , flow right [plainText "you got ", plainText . String.left 4 . show <| state.person.alc, plainText " grams of unabsorbed ethanol in ur belly"]
-              , flow right [plainText "ur bac is: ", plainText . String.left 6 . show <| state.person.bac]
+              , flow right [plainText "you are an ", plainText << toString <| state.person.weight, plainText "kg ", plainText << toString <| state.person.sex]
+              , flow right [plainText "your current beer of choice is ", plainText << toString <| (snd state.person.beers).name]
+              , flow right [plainText "of which you have ", plainText << toString <| fst state.person.beers, plainText " ml left in the glass"]
+              , flow right [plainText "you got ", plainText << String.left 4 << toString <| state.person.alc, plainText " grams of unabsorbed ethanol in ur belly"]
+              , flow right [plainText "ur bac is: ", plainText << String.left 6 << toString <| state.person.bac]
               , plainText <| peeDisplay state.person.urine state.person.wetSelf ++ (if state.person.urinating then " (you are peeing)" else "")
-              , flow right [plainText "you've had ", plainText . show <| state.drinks, plainText " beers"]
+              , flow right [plainText "you've had ", plainText << toString <| state.drinks, plainText " beers"]
               , flow right [plainText "u been at the bar for: ", plainText <| timeDisplay state.elapsed]
               , flow right [sipButton, gulpButton, urinateButton]
               , flow right [orderButton, orderButton2]
@@ -103,13 +114,13 @@ timeDisplay : Time -> String
 timeDisplay t =
     let t' = t * 3600000
         hours = floor <| inHours t'
-        minutes = mod (floor <| inMinutes t') 60
-        seconds = mod (floor <| inSeconds t') 60
-    in String.padLeft 2 '0' (show hours)
+        minutes = (floor <| inMinutes t') % 60
+        seconds = (floor <| inSeconds t') % 60
+    in String.padLeft 2 '0' (toString hours)
     ++ ":"
-    ++ String.padLeft 2 '0' (show minutes)
+    ++ String.padLeft 2 '0' (toString minutes)
     ++ ":"
-    ++ String.padLeft 2 '0' (show seconds)
+    ++ String.padLeft 2 '0' (toString seconds)
 
 peeDisplay : Float -> Bool -> String
 peeDisplay urine wetSelf =
